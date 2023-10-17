b899ad2aa80ce43bdc257d4691c64f8f152640fb
M_Mek <mmekraz781@gmail.com>
2023-10-17 10:50:54 -0400

    initial commit

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ var cat = document.querySelector('.cat');
 button.addEventListener('click', function () {
     if (cat.classList.contains('show')) {
         cat.classList.remove('show')
+        button.classList.remove('disappear')
         button.innerText = 'BRING IT BACK!'
     } else {
     cat.classList.add('show')
+    button.classList.add('disappear')
     button.innerText = 'Hide the Kitty'}
 })
